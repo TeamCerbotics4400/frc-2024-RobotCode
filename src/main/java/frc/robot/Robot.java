a690a9ca65136d29eb62420aea6b4955c5d31660
SvenXD <Romanhernandezsosa1@gmail.com>
2024-01-11 19:28:53 -0600

    Changed Falcon motors for Neo motors

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -24,7 +24,6 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
   DoubleLogEntry batteryVoltage;
-
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
