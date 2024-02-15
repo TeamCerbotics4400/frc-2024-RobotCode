90a7d1536e273bb9ae25ca919143d7ee78859150
SvenXD <Romanhernandezsosa1@gmail.com>
2024-02-15 16:32:56 -0600

    Added path for the current robot

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -117,7 +117,7 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return new PathPlannerAuto("Traverse Auto Test");//m_autoChooser.getSelected();
+    return new PathPlannerAuto("Traverse for old intake test");//m_autoChooser.getSelected();
   }
 
   public DriveTrain getDrive(){
